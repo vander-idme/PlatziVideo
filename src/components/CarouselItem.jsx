2666f61447710a127f9ef9d51a8f8554a0ccab62
vander-idme <idmevander@gmail.com>
2020-08-09 10:05:31 -0500

    Arreglando typo en el archivo

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -14,7 +14,7 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
       </div>
 <p className="carousel-item__details--title">{title}</p>
       <p className="carousel-item__details--subtitle">
-        {`${year} ${contentRating} ${duration}`}
+        {`${year} ${contentRating} ${duration} min`}
       </p>
     </div>
   </div>
